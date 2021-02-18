518b195c929108b00216d46878b05ca3168cc1ec
Afonso Santos <afonso.faisca1@gmail.com>
2021-02-18 16:57:19 +0000

    18/02/2021 class

--- carros/marcas.php
+++ carros/marcas.php
@@ -1,3 +1,10 @@
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		require('validacao.php');
+		$validacao = validarFormulario();
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -26,18 +33,42 @@
 		</ul>
 	</nav>
 
-	<form method="post" action="">
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 		<table>
 			<tr>
 				<td width="130">Marca: </td>
 				<td width="130">
-					<input type="text" name="marca" size="20" placeholder="Marca do carro">
+					<input
+						type="text"
+						name="marca"
+						size="20"
+						placeholder="Marca do carro"
+						value="<?php if($_POST && !empty($_POST['marca'])){echo $_POST['marca'];} ?>">
+
+					<!-- Validação -->
+					<?php
+						if(isset($_POST['enviar']) && in_array('marca', $validacao)) {
+							echo '<span class="erro">Preenchimento obrigatório!</span>';
+						}
+					?>
 				</td>
 			</tr>
 			<tr>
 				<td width="130">País de Origem: </td>
 				<td width="130">
-					<input type="text" name="pais" size="20" placeholder="País de origem do carro">
+					<input
+						type="text"
+						name="pais"
+						size="20"
+						placeholder="País de origem do carro"
+						value="<?php if ($_POST && !empty($_POST['pais'])){echo $_POST['pais'];}?>">
+
+					<!-- Validação -->
+					<?php
+						if(isset($_POST['enviar']) && in_array('pais', $validacao)) {
+							echo '<span class="erro">Preenchimento obrigatório!</span>';
+						}
+					?>
 				</td>
 			</tr>
 			<tr>
@@ -54,5 +85,24 @@
 			</tr>
 		</table>
 	</form>
+
+	<?php
+		if ($_POST && count($validacao) == 0) {
+			echo '<br />Dados enviados!<br />';
+
+			$conn = new mysqli('localhost:3306', 'root', '', 'desweb_cars');
+
+			if ($con->connect_errno) {
+				echo 'Ocorreu um erro ao ligar à BD ' . $conn->connect_error;
+				exit();
+			}
+
+			echo 'Ligação à BD com sucesso!';
+
+			if (isset($_POST['enviar'])) {
+				# TODO
+			}
+		}
+	?>
 </body>
 </html>
